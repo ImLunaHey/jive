09825ed066009a83b9cec053856484943f71e3c4
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-03-05 12:47:33 +1030

    chore: better logging for checking feature enabled

--- src/common/is-feature-enabled.ts
+++ src/common/is-feature-enabled.ts
@@ -6,30 +6,32 @@ type Autocomplete<Keys extends string> = Keys | Omit<string, Keys>;
 const globallyEnabled: string[] = [];
 
 export const isFeatureEnabled = async (id: Autocomplete<'leveling' | 'welcome'>, guildId?: string) => {
-    globalLogger.debug('Checking if feature is enabled', { id, guildId });
+    const check = async () => {
+        if (!guildId) return false;
 
-    if (!guildId) return false;
+        // If the feature is enabled globally, return true
+        if (globallyEnabled.includes(id as string)) return true;
 
-    // If the feature is enabled globally, return true
-    if (globallyEnabled.includes(id as string)) return true;
+        // If this feature doesn't exist, return false
+        if (!['leveling', 'welcome'].includes(id as string)) return false;
 
-    // If this feature doesn't exist, return false
-    if (!['leveling', 'welcome'].includes(id as string)) return false;
-
-    // Is this feature enabled for this guild?
-    const features = await prisma.features.findFirst({
-        where: {
-            guild: {
-                id: guildId
+        // Is this feature enabled for this guild?
+        const features = await prisma.features.findFirst({
+            where: {
+                guild: {
+                    id: guildId
+                }
+            },
+            select: {
+                leveling: true,
+                welcome: true,
+                autoRoles: true,
+                starboard: true,
             }
-        },
-        select: {
-            leveling: true,
-            welcome: true,
-            autoRoles: true,
-            starboard: true,
-        }
-    });
+        });
 
-    return features?.[id as keyof typeof features].enabled;
+        return features?.[id as keyof typeof features].enabled;
+    };
+
+    globalLogger.debug('Checking feature enabled', { id, guildId, enabled: await check() });
 };
