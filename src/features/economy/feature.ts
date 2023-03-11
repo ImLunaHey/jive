805f4bbdf44ee03592b5f3aa9c3c6ce268c5cba7
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-03-12 00:47:28 +1030

    fix: update not reply

--- src/features/economy/feature.ts
+++ src/features/economy/feature.ts
@@ -505,7 +505,7 @@ export class Feature {
                 });
 
                 // Show the attack
-                await interaction[interaction.replied ? 'editReply' : 'reply']({
+                await interaction.update({
                     embeds: [{
                         title: 'Encounter',
                         description: outdent`
@@ -526,7 +526,7 @@ export class Feature {
                 const nextInitiative = encounter.initatives[encounter.turn + 1] ?? encounter.initatives[0];
 
                 // Show them a list of actions they can take
-                await interaction[interaction.replied ? 'editReply' : 'reply']({
+                await interaction.update({
                     embeds: [{
                         title: 'Encounter',
                         fields: [{
