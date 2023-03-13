b8223b230b3b1539a737a0be4781c7e3793b5fcc
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-03-13 21:12:17 +1030

    fix: always exit bot on error

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,5 @@ import { globalLogger } from '@app/logger';
 start().catch((error: unknown) => {
     if (!(error instanceof Error)) throw new Error(`Unknown error "${String(error)}"`);
     globalLogger.error('Failed to load bot with "%s"\n%s', error.message, error.stack);
+    process.exit(1);
 });
