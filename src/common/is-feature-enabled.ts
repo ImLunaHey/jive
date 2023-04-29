0014fd4d1bef5f746bc631cd60b57769f5df3187
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-04-29 21:40:55 +0930

    chore: more logging

--- src/common/is-feature-enabled.ts
+++ src/common/is-feature-enabled.ts
@@ -4,28 +4,37 @@ import { globalLogger } from '@app/logger';
 
 const globallyEnabled: Feature[] = [];
 
+const check = async (feature: Feature, guildId?: string) => {
+    if (!guildId) return false;
+
+    // If the feature is enabled globally, return true
+    if (globallyEnabled.includes(feature)) return true;
+
+    // Is this feature enabled for this guild?
+    const settings = await db
+        .selectFrom('settings')
+        .select('featuresEnabled')
+        .where('guildId', '=', guildId)
+        .executeTakeFirst();
+
+    try {
+        return settings?.featuresEnabled.includes(feature);
+    } catch (error) {
+        globalLogger.error(error);
+
+        return false;
+    }
+};
+
 export const isFeatureEnabled = async (feature: Feature, guildId?: string) => {
-    const check = async () => {
-        if (!guildId) return false;
+    // Check if the feature is enabled
+    const enabled = await check(feature, guildId);
 
-        // If the feature is enabled globally, return true
-        if (globallyEnabled.includes(feature)) return true;
+    globalLogger.info('Checked if feature is enabled', {
+        feature,
+        guildId,
+        enabled,
+    });
 
-        // Is this feature enabled for this guild?
-        const settings = await db
-            .selectFrom('settings')
-            .select('featuresEnabled')
-            .where('guildId', '=', guildId)
-            .executeTakeFirst();
-
-        try {
-            return settings?.featuresEnabled.includes(feature);
-        } catch (error) {
-            globalLogger.error(error);
-
-            return false;
-        }
-    };
-
-    return await check();
+    return enabled;
 };
