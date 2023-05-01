cd27c133cd0db2bc999e49c55d97099f3434e9ce
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-05-01 17:39:46 +0930

    fix: negative memberMissing count

--- src/features/stats/feature.ts
+++ src/features/stats/feature.ts
@@ -42,7 +42,7 @@ export class Feature {
             this.logger.info('Backfilling members for guild', {
                 guildId: guild.id,
                 memberCount: guild.memberCount,
-                membersMissing: (guildMembers?.memberCount ?? guild.memberCount) - guild.memberCount,
+                membersMissing: guild.memberCount - (guildMembers?.memberCount ?? guild.memberCount),
                 membersExisting: guildMembers?.memberCount ?? 0,
             });
 
